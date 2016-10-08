23ce152ec7a693d92e9a191f2184da790b25c46d
Petr Pogorelov <peter.pogorelov@gmail.com>
2016-10-08 18:53:53 +0500

    Fxd.

--- src/main/java/storycommands/CommandSave.java
+++
@@ -1,7 +0,0 @@
-package storycommands;
-
-/**
- * Created by Petr on 07.10.2016.
- */
-public class CommandSave {
-}
--- src/main/java/storycommands/choosecommands/CommandChooseThird.java
+++
@@ -1,7 +0,0 @@
-package storycommands.choosecommands;
-
-/**
- * Created by Petr on 07.10.2016.
- */
-public class CommandChooseThird {
-}
